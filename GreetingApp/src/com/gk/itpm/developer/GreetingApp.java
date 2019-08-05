e64c85a50c73f065ff0bc8252e335dfc0c1ee011
user <user@user-THINK>
2019-08-05 10:06:37 +0900

    greetingAppを課題に合わせて改変

--- GreetingApp/src/com/gk/itpm/developer/GreetingApp.java
+++ GreetingApp/src/com/gk/itpm/developer/GreetingApp.java
@@ -4,12 +4,18 @@ public class GreetingApp {
 
 	public static void main(String[] args) {
 
+		//雛形メッセージを作成する
+		String greeting =
+				"私の名前は%sです。私の好きなものは%sです。成人式を迎えて%d年過ぎました。";
+
+
 		String name = "黄　顔甫";
 		String myFavorite  = "湯豆腐";
+		int age = 35;
 
-		System.out.println("私の名前は" + name + "です。");
-		System.out.println("私の好きなものは" + myFavorite + "です。");
-		System.out.println("よろしくお願いいたします。");
+		String greetingMsg = String.format(greeting, name, myFavorite, age - 20);
+
+		System.out.println(greetingMsg);
 
 	}
 
